b1136fbf5d82d1aa895888151af5d9f8ec474e79
judetensor <ebukaonuh68@gmail.com>
2020-08-15 11:00:49 +0100

    added semicolon at alert(); i initially forgot to include it

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-alert("Hello!!! Top of the day to you")
+alert("Hello!!! Top of the day to you");
 var password = prompt("Please input your Password");
 if (password == "admin") {
   alert("Correct Password");
